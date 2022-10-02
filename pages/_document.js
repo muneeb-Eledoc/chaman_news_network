1b59b272f6c7c011afd06f391a472f9f82524ffb
unknown <muneebjs.css@gmail.com>
2022-10-02 19:02:46 +0500

    Google Verification

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <meta name="google-site-verification" content="kcYzfPJkKq9bMhND4L3ZF_yvEbBZn1UtMmXq8PDuX0I" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6770516049938947"
         crossorigin="anonymous"></script>
 
